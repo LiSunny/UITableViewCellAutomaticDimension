ad58a7ffbfdbf59058ebaa032dced3edac752ed2
liyang <ccli321@163.com>
2018-03-02 08:33:01 +0800

    Initial Commit

---
+++ AutoFixCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoFixCell
+//
+//  Created by Yang Blus on 2018/3/2.
+//  Copyright © 2018年 Yang Blus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoFixCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoFixCell
+//
+//  Created by Yang Blus on 2018/3/2.
+//  Copyright © 2018年 Yang Blus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
